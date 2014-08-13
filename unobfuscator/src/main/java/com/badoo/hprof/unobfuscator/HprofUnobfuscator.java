99fc450c81164fe7ec6385376a5727ab69fa62a1
Erik Andre <erik.andre@corp.badoo.com>
2014-08-13 15:21:49 +0100

    Main class for unobf

---
+++ unobfuscator/src/main/java/com/badoo/hprof/unobfuscator/HprofUnobfuscator.java
@@ -0,0 +1,11 @@
+package com.badoo.hprof.unobfuscator;
+
+/**
+ * Created by Erik Andre on 13/08/2014.
+ */
+public class HprofUnobfuscator {
+
+    public static void main(String args[]) {
+    }
+
+}
