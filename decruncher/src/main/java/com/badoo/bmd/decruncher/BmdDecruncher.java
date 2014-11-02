95426e85986c3ff414dcd64d731a56c9ddc58ed7
Erik Andre <erik.andre@corp.badoo.com>
2014-11-02 20:57:27 +0000

    Added module for decruncher

---
+++ decruncher/src/main/java/com/badoo/bmd/decruncher/BmdDecruncher.java
@@ -0,0 +1,11 @@
+package com.badoo.bmd.decruncher;
+
+/**
+ * Created by Erik Andre on 02/11/14.
+ */
+public class BmdDecruncher {
+
+    public static void main(String[] args) {
+        
+    }
+}
