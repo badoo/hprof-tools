051829f4b97f48e400ecd5a63c78028c0061e081
Erik Andre <erik.andre@corp.badoo.com>
2015-11-20 10:29:19 +0000

    Added module for hprof-viewer

---
+++ hprof-viewer/src/main/java/com/badoo/hprof/viewer/HprofViewer.java
@@ -0,0 +1,18 @@
+package com.badoo.hprof.viewer;
+
+/**
+ * Entry point for the HPROF Viewer application.
+ *
+ * HPROF Viewer supports the following functionality:
+ *
+ * TODO
+ *
+ * Created by Erik Andre on 20/11/15.
+ */
+public class HprofViewer {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
